72bbc36144a6dd0137b4e390f4f5c8353c82776c
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-08-08 14:27:03 +0100

    add electric car post

--- src/components/mdxlayout.jsx
+++ src/components/mdxlayout.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 // https://www.gatsbyjs.com/docs/how-to/routing/customizing-components/
 
 export function Layout({ children }) {
-  const data = useStaticQuery(graphql`
-    query {
-      mdx {
-        frontmatter {
-          description
+    const data = useStaticQuery(graphql`
+        query {
+            mdx {
+                frontmatter {
+                    description
+                }
+            }
         }
-      }
-    }
-  `);
+    `);
 
-  const { description } = data.mdx.frontmatter;
+    const { description } = data.mdx.frontmatter;
 
-  return (
-    <>
-      <header>My Header</header>
+    return (
+        <>
+            <header>My Header</header>
 
-      <p>{description}</p>
+            <p>{description}</p>
 
-      <div
-        className="text-base mx-auto w-full max-w-prose"
-        id="mdx-container-div"
-      >
-        {children}
-      </div>
-      <footer>My Footer</footer>
-    </>
-  );
+            <div
+                className="text-base mx-auto w-full max-w-prose px-4"
+                id="mdx-container-div"
+            >
+                {children}
+            </div>
+            <footer>My Footer</footer>
+        </>
+    );
 }
 
 export default Layout;
