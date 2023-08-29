c6ff35120ce5b2dcf36a033750dffcfad4f1d82e
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-08-29 16:05:24 +0100

    simplified

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,13 +40,6 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "non_post_mdx",
-        "path": "./src/other/"
-      }
-    },
-    {
       resolve: `gatsby-plugin-page-creator`,
       options: {
         path: `${__dirname}/src/mdx`,
