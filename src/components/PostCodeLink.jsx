39dabe0cf1ed8c033d841bcd9f5e82549f33bd63
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-09-01 19:59:57 +0100

    nicer links

--- src/components/PostCodeLink.jsx
+++ src/components/PostCodeLink.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 const PostCodeLink = ({ frontmatter }) => {
     const { post_date, code_url } = frontmatter;
+    let text = 'View source code for this page';
+    if (code_url.includes('observablehq.com')) {
+        text = 'Live edit this notebook';
+    }
 
     return (
-        <p>
-            Originally posted: {post_date}. View{' '}
-            <a href={code_url} target="_blank" rel="noopener noreferrer">
-                source code for this page
-            </a>{' '}
-            here.
+        <p className="text-gray-400 text-sm">
+            Originally posted: {post_date}. {text}{' '}
+            <a
+                href={code_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 text-sm"
+            >
+                here.
+            </a>
         </p>
     );
 };
