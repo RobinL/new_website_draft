2c5142938d810060dc0f50b4d9e8e9b208a32616
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-09-01 13:58:20 +0100

    pale link

--- src/components/mdxlayout.jsx
+++ src/components/mdxlayout.jsx
@@ -2,28 +2,18 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Header from './Header';
 import Footer from './Footer';
+import PostInfo from './PostCodeLink';
 
 // https://www.gatsbyjs.com/docs/how-to/routing/customizing-components/
 
-export function Layout({ children }) {
-    const data = useStaticQuery(graphql`
-        query {
-            mdx {
-                frontmatter {
-                    description
-                    code_url
-                }
-            }
-        }
-    `);
-
-    const { description, code_url } = data.mdx.frontmatter;
+export function Layout({ children, pageContext }) {
+    const { frontmatter } = pageContext;
 
     return (
         <>
             <div className="text-base mx-auto w-full max-w-prose px-4">
                 <Header />
-                <div>{code_url}</div>
+                <PostInfo frontmatter={frontmatter} />
                 <div id="mdx-container-div">{children}</div>
                 <Footer />
             </div>
