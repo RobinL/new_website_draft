619d3857a792ddc723875549243a52861104806e
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-09-07 09:15:32 +0100

    add observable cells

---
+++ src/components/ObservableCells.jsx
@@ -0,0 +1,66 @@
+import React, {
+    useRef,
+    useEffect,
+    createContext,
+    useContext,
+    useState,
+} from 'react';
+import { Runtime, Inspector, Library } from '@observablehq/runtime';
+
+const ObservableRuntimeContext = createContext(null);
+
+const mountId = 'mdx-container-div';
+
+const stdlib = new Library();
+
+const library = Object.assign({}, stdlib, { width });
+
+function width() {
+    return stdlib.Generators.observe(notify => {
+        let width = notify(document.getElementById(mountId).clientWidth);
+        function resized() {
+            let width1 = document.getElementById(mountId).clientWidth;
+            if (width1 !== width) notify((width = width1));
+        }
+
+        window.addEventListener('resize', resized);
+        return () => window.removeEventListener('resize', resized);
+    });
+}
+
+export function ObservableProvider({ notebook, children }) {
+    const [sharedRefs, setSharedRefs] = useState({});
+    const runtime = new Runtime(Object.assign({}, new Library(), { width }));
+
+    useEffect(() => {
+        runtime.module(notebook, name => {
+            if (sharedRefs[name]) {
+                return new Inspector(sharedRefs[name]);
+            }
+        });
+        return () => runtime.dispose();
+    }, [runtime, sharedRefs, notebook]);
+
+    return (
+        <ObservableRuntimeContext.Provider value={{ setSharedRefs }}>
+            {children}
+        </ObservableRuntimeContext.Provider>
+    );
+}
+
+export function ObservableCell({ cellName }) {
+    const ref = useRef(null);
+    const { setSharedRefs } = useContext(ObservableRuntimeContext);
+
+    useEffect(() => {
+        setSharedRefs(prevRefs => ({ ...prevRefs, [cellName]: ref.current }));
+    }, [cellName, setSharedRefs]);
+
+    return <div ref={ref} />;
+}
+
+export function WithObservableProvider({ notebook, children }) {
+    return (
+        <ObservableProvider notebook={notebook}>{children}</ObservableProvider>
+    );
+}
--- src/components/ObservableNotebook.jsx
+++ src/components/ObservableNotebook.jsx
@@ -1,46 +1,46 @@
-import React, { useRef, useEffect } from "react";
-import { Runtime, Inspector, Library } from "@observablehq/runtime";
+import React, { useRef, useEffect } from 'react';
+import { Runtime, Inspector, Library } from '@observablehq/runtime';
 
-const mountId = "mdx-container-div";
+const mountId = 'mdx-container-div';
 
 const stdlib = new Library();
 
 const library = Object.assign({}, stdlib, { width });
 
 function width() {
-  return stdlib.Generators.observe((notify) => {
-    let width = notify(document.getElementById(mountId).clientWidth);
-    function resized() {
-      let width1 = document.getElementById(mountId).clientWidth;
-      if (width1 !== width) notify((width = width1));
-    }
+    return stdlib.Generators.observe(notify => {
+        let width = notify(document.getElementById(mountId).clientWidth);
+        function resized() {
+            let width1 = document.getElementById(mountId).clientWidth;
+            if (width1 !== width) notify((width = width1));
+        }
 
-    window.addEventListener("resize", resized);
-    return () => window.removeEventListener("resize", resized);
-  });
+        window.addEventListener('resize', resized);
+        return () => window.removeEventListener('resize', resized);
+    });
 }
 
 function ObservableNotebook({ notebook, cells }) {
-  const refs = useRef(cells.map(() => React.createRef()));
+    const refs = useRef(cells.map(() => React.createRef()));
 
-  useEffect(() => {
-    const runtime = new Runtime(library);
-    runtime.module(notebook, (name) => {
-      const index = cells.indexOf(name);
-      if (index !== -1) {
-        return new Inspector(refs.current[index].current);
-      }
-    });
-    return () => runtime.dispose();
-  }, [notebook, cells]);
+    useEffect(() => {
+        const runtime = new Runtime(library);
+        runtime.module(notebook, name => {
+            const index = cells.indexOf(name);
+            if (index !== -1) {
+                return new Inspector(refs.current[index].current);
+            }
+        });
+        return () => runtime.dispose();
+    }, [notebook, cells]);
 
-  return (
-    <>
-      {refs.current.map((ref, index) => (
-        <div key={index} ref={ref} />
-      ))}
-    </>
-  );
+    return (
+        <>
+            {refs.current.map((ref, index) => (
+                <div key={index} ref={ref} />
+            ))}
+        </>
+    );
 }
 
 export default ObservableNotebook;
